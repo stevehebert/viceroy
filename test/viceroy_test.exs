05206cc263923f616731ab3e3d32bc8be8ccba36
Steve Hebert <steve.hebert@gmail.com>
2019-08-08 11:17:02 -0500

    fixed compile and format error

--- test/viceroy_test.exs
+++ test/viceroy_test.exs
@@ -7,22 +7,22 @@ defmodule ViceroyTest do
   end
 
   test "triangle left test" do
-    assert TriangleMembership.resolve({0,5,10}, 2.5) == 0.5
+    assert TriangleMembership.resolve({0, 5, 10}, 2.5) == 0.5
   end
 
   test "triangle right test" do
-    assert TriangleMembership.resolve({0,5,10}, 6) == 0.8
+    assert TriangleMembership.resolve({0, 5, 10}, 6) == 0.8
   end
 
   test "triangle mid test" do
-    assert TriangleMembership.resolve({0,5,10}, 5) == 1.0
+    assert TriangleMembership.resolve({0, 5, 10}, 5) == 1.0
   end
 
   test "triangle lower bound test" do
-    assert TriangleMembership.resolve({3,5,10}, 1) == 0
+    assert TriangleMembership.resolve({3, 5, 10}, 1) == 0
   end
 
   test "triangle upper bound test" do
-    assert TriangleMembership.resolve({3,5,10}, 20) == 0
+    assert TriangleMembership.resolve({3, 5, 10}, 20) == 0
   end
 end
