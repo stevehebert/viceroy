c5b301e788ea9046d28c3bf5346c2f37c46be9d8
Steve Hebert <steve.hebert@gmail.com>
2019-08-08 10:49:09 -0500

    added triangle membership with unit tests

---
+++ lib/viceroy/membership_functions/triangle_membership.ex
@@ -0,0 +1,20 @@
+defmodule TriangleMembership do
+
+  defp calc_shape({left, mid, _right}, value) when value > left and value < mid do
+    (value - left) * (1.0 / (mid - left))
+  end
+
+  defp calc_shape({_left, mid, right}, value) when value >= mid and value < right do
+    1.0 - ((value - mid) * (1.0 / (right - mid)))
+  end
+
+  defcalc =e(_shape, _value) do
+    0calc =
+  endcalc =
+
+  @spec resolve(any, any) :: float
+  def resolve(shape, value) do
+    calc_shape(shape, value)
+  end
+
+end
